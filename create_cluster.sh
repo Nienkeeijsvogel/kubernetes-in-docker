a0fe28cd585415df5d2789c2e1d7e3af6fdbfc73
Nienkeeijsvogel <32362037+Nienkeeijsvogel@users.noreply.github.com>
2022-05-29 19:46:22 +0200

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -10,7 +10,6 @@ docker exec master kubeadm init --ignore-preflight-errors all \
                                 --pod-network-cidr 10.244.0.0/16 \
                                 --token dzsner.a8tyt63f4hbs2ukz \
                                  
-
 docker exec master sh -c "kubectl get configmap -n kube-system coredns -o yaml | sed '/loop/d' | kubectl replace -f -"
 
 # Create pod network
@@ -18,20 +17,24 @@ docker exec master kubectl apply -f https://raw.githubusercontent.com/cloudnativ
 docker exec master kubectl -n kube-system delete ds kube-proxy
 
 # Create worker nodes
-docker run -d --privileged --network kafka --name zookeeper1 icebert/kubernetes-in-docker
-docker run -d --privileged --network kafka --name kafka-1 icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name zookeeper icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name kafka icebert/kubernetes-in-docker
 docker run -d --privileged --network kafka --name producer icebert/kubernetes-in-docker
 docker run -d --privileged --network kafka --name consumer icebert/kubernetes-in-docker
+ 
+docker exec zookeeper kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
+                                             --discovery-token-unsafe-skip-ca-verification \
+                                             --ignore-preflight-errors all
+docker exec kafka kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
+                                             --discovery-token-unsafe-skip-ca-verification \
+                                             --ignore-preflight-errors all
+docker exec producer kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
+                                             --discovery-token-unsafe-skip-ca-verification \
+                                             --ignore-preflight-errors all
+docker exec consumer kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
+                                             --discovery-token-unsafe-skip-ca-verification \
+                                             --ignore-preflight-errors all
 
-docker exec worker0 kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
-                                             --discovery-token-unsafe-skip-ca-verification \
-                                             --ignore-preflight-errors all
-docker exec worker1 kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
-                                             --discovery-token-unsafe-skip-ca-verification \
-                                             --ignore-preflight-errors all
-docker exec worker2 kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
-                                             --discovery-token-unsafe-skip-ca-verification \
-                                             --ignore-preflight-errors all
 
 
 
