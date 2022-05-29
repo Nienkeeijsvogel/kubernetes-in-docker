6265d0884ca511713e05154d73763083fb1953d1
Nienkeeijsvogel <32362037+Nienkeeijsvogel@users.noreply.github.com>
2022-05-29 18:51:36 +0200

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create network
-docker network create k8s-net
+docker network create kafka
 
 # Create control-plane
-docker run -d --privileged --network k8s-net --name master icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name master icebert/kubernetes-in-docker
 
 docker exec master kubeadm init --ignore-preflight-errors all \
                                 --pod-network-cidr 10.244.0.0/16 \
                                 --token dzsner.a8tyt63f4hbs2ukz \
-                                --image-repository gcr.azk8s.cn/google_containers
+                                 
 
 docker exec master sh -c "kubectl get configmap -n kube-system coredns -o yaml | sed '/loop/d' | kubectl replace -f -"
 
@@ -18,9 +18,10 @@ docker exec master kubectl apply -f https://raw.githubusercontent.com/cloudnativ
 docker exec master kubectl -n kube-system delete ds kube-proxy
 
 # Create worker nodes
-docker run -d --privileged --network k8s-net --name worker0 icebert/kubernetes-in-docker
-docker run -d --privileged --network k8s-net --name worker1 icebert/kubernetes-in-docker
-docker run -d --privileged --network k8s-net --name worker2 icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name zookeeper1 icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name kafka-1 icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name producer icebert/kubernetes-in-docker
+docker run -d --privileged --network kafka --name consumer icebert/kubernetes-in-docker
 
 docker exec worker0 kubeadm join master:6443 --token dzsner.a8tyt63f4hbs2ukz \
                                              --discovery-token-unsafe-skip-ca-verification \
